f5e4ca4c6b1dc18145415c2092370a7d104cf567
zanlaihu <HUZA1_15@uni.worc.ac.uk>
2016-10-20 16:04:14 +0100

    1

--- www/js/index.js
+++ www/js/index.js
@@ -1,41 +1,27 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+
 
     function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-		console.log("zanlai's device ready");
-    }
-	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
-
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-		alert("device ready");
+        var simpsons = {
+            "firstName": "Homer",
+            "lastName": "Simpson",
+            "address": {
+                "streetAddress": "742 Evergreen Terrace",
+                "city": "Springfield",
+                "state": "?"
+            }
+        }
         
-		document.addEventListener("resume", onResume, false);
-		document.addEventListener("pause", onPause, false);
-		
-		launched_count++;
-		updateDisplay();
-    }
+    console.log(simpsons.firstName +" is from " + simpsons.address.city);
+        
+    var homerString = JSON.stringify(simpsons);
+        
+    window.localStorage.setItem(“address”, homerString);
+        
+    var getSimpson = window.localStorage.getItem(“address”);
 
-    // Handle the pause event
-    //
-    function onPause() {
-		alert("pause");
-		paused_count++;
-		updateDisplay();
+    var simpsonJSON = JSON.parse(getSimpson);
+        
+    alert(simpsons.address.city);
     }
 	
-	function onResume() {
-		alert("resume");
-		resumed_count++;
-		updateDisplay();
-    }
+	
