b27f5151b769af16e16b749150c696e10d3742c3
zanlaihu <HUZA1_15@uni.worc.ac.uk>
2016-10-20 16:27:12 +0100

    5

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,6 @@
+var paused_count =0;
+var resumed_count = 0;
+var launched_count = 0;
 
 
     function onLoad() {
@@ -21,8 +24,40 @@
 
     var simpsonJSON = JSON.parse(getSimpson);
         
-    alert(simpsons.address.city);
+    alert(simpsonsJSON.address.city);
         
     }
 	
+function updateDisplay() {
+		$("#launched").text("Application launched: " + launched_count);
+		$("#resumed").text("Application paused: " + paused_count);
+		$("#paused").text("Application resumed: " + resumed_count);
+	}
+
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+		alert("device ready");
+        
+		document.addEventListener("resume", onResume, false);
+		document.addEventListener("pause", onPause, false);
+		
+		launched_count++;
+		updateDisplay();
+    }
+
+    // Handle the pause event
+    //
+    function onPause() {
+		alert("pause");
+		paused_count++;
+		updateDisplay();
+    }
+	
+	function onResume() {
+		alert("resume");
+		resumed_count++;
+		updateDisplay();
+    }
 	
